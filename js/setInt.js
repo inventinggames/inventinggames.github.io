fdab3b7d6f10db36aeb5ef79b30e6cdc7f21ea9f
Krupsky, Alexander <akrupsky@scnsoft.com>
2019-09-02 14:22:34 +0300

    rebuilding site Mon, Sep  2, 2019  2:22:34 PM

--- js/setInt.js
+++ js/setInt.js
@@ -1,13 +1,48 @@
 (function() {
     var last;
+    var delay = false;
    
+    document.getElementById('delay-button').addEventListener('click', function(e){
+        delay = !delay;
+        if (delay){
+            this.style.backgroundColor = "#FE5F55";
+        }else{
+            this.style.backgroundColor = "";
+        }
+    })
+    
+
+    function sleep(milliseconds) {
+        var start = new Date().getTime();
+        for (var i = 0; i < 1e7; i++) {
+            if ((new Date().getTime() - start) > milliseconds){
+            break;
+            }
+        }
+    }
+
     var draw = function(){
         var time = performance.now();
         var frameTime = (time - last).toFixed(2);
         last = time;
 
+        if (delay){
+            sleep(20);
+        }
+
         document.getElementsByClassName('clear-frame-rate')[0].innerText = 'FPS: ' + Math.round(1000/Number(frameTime));
     }
+
+    var lastAnim;
+    var drawAnim = function(timeAnim){
+        var frameTimeAnim = (timeAnim - lastAnim).toFixed(2);
+        lastAnim = timeAnim;
+        
+        document.getElementsByClassName('anim-frame-rate')[0].innerText = 'FPS: ' + Math.round(1000/Number(frameTimeAnim));
+
+        window.requestAnimationFrame(drawAnim);
+    }
+    window.requestAnimationFrame(drawAnim)
 
     var int = setInterval(draw, 1000/60);
 })();
