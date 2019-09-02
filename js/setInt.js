1422c35888e72f8b30d31b5f999248e9ce15f326
Krupsky, Alexander <akrupsky@scnsoft.com>
2019-09-02 16:50:57 +0300

    rebuilding site Mon, Sep  2, 2019  4:50:57 PM

--- js/setInt.js
+++ js/setInt.js
@@ -1,16 +1,26 @@
 (function() {
     var last;
     var delay = false;
-   
-    document.getElementById('delay-button').addEventListener('click', function(e){
+    var delayAnim = false;
+
+    var onDelayButtonClick = function(){
         delay = !delay;
-        if (delay){
-            this.style.backgroundColor = "#FE5F55";
-        }else{
-            this.style.backgroundColor = "";
+        
+        var buttons = document.getElementsByClassName('delay-button');
+        for (var i = 0; i < buttons.length; i++){
+            if (delay){
+                buttons[i].style.backgroundColor = "#FE5F55";
+            }else{
+                buttons[i].style.backgroundColor = "";
+            }
         }
-    })
-    
+        
+    }
+   
+    var buttons = document.getElementsByClassName('delay-button');
+    for (var i = 0; i < buttons.length; i++){
+        buttons[i].addEventListener('click', onDelayButtonClick)
+    }
 
     function sleep(milliseconds) {
         var start = new Date().getTime();
@@ -40,9 +50,13 @@
         
         document.getElementsByClassName('anim-frame-rate')[0].innerText = 'FPS: ' + Math.round(1000/Number(frameTimeAnim));
 
+        if (delay){
+            sleep(20);
+        }
+
         window.requestAnimationFrame(drawAnim);
     }
     window.requestAnimationFrame(drawAnim)
 
-    var int = setInterval(draw, 1000/60);
+    setInterval(draw, 1000/60);
 })();
